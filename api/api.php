d294826cef726377fa3683e0290639518eefd382
Michael Flynn <mflynn@cngann.com>
2015-04-04 13:48:23 -0700

    Adding phpStorm project files

--- api/api.php
+++ api/api.php
@@ -5,7 +5,7 @@
  * @author     Michael Flynn <mike@clearcode.info>
  * @author     Chris Gann <chris@clearcode.info>
  * @copyright  2015 C.N.Gann Technology Group L.L.C.
- * @version    SVN: $Id$
+ * @version    1.0
  */
 
 require_once ( 'defines.php' );
